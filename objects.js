42e608bb1f45518a6a5eaf79224df891affaab93
Cliff Jurgensen <jivefox.002@gmail.com>
2018-10-03 23:27:02 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'artistName': 'songTitle'}
+
+function updatePlaylist(poopy, poop, poopie) {
+ poopy[poop] = poopie;
+  return playlist;
+}
+function removeFromPlaylist(playlist, key) {
+  delete playlist.key;
+  return playlist;
+}
